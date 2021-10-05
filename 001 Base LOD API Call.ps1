840c1332a853973536c0a4bdf0eb760a5cab9fc0
paulieguk <paul.gregory@learnondemandsystems.com>
2021-10-05 14:42:08 +0100

    Add files via upload

---
+++ 001 Base LOD API Call.ps1
@@ -0,0 +1,15 @@
+﻿## Set your api key, site url, Be sure to remove the <>Brackets when adding the api key.
+$api_key = @{'api_key' = '<API key goes here>'}
+$baseURL = 'https://labondemand.com/api/v3'
+$apiCMD = 'catalog'
+
+## Powershell does splatting pretty well, these are the parameters for the api call.
+$apiCall = @{
+        Method = "Get"
+        Uri =  "$($baseURL)/$($apiCMD)"
+        ContentType = "application/json"
+        Headers = $api_key
+    }
+
+## This is the actual API Call to get the class as it is currently.
+$apiResponse = Invoke-RestMethod @apiCall
